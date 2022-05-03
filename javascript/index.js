aa9a947963628408932cf19698eb4ddd7d77fb43
Samy Ali <samy.alisanchez91@gmail.com>
2022-05-03 08:03:32 +0200

    done

--- javascript/index.js
+++ javascript/index.js
@@ -130,6 +130,7 @@ obtainInstruction("steak", 0)
   .then(() => obtainInstruction("steak", 7))
   .then(() => {
     document.querySelector("#steak").innerHTML += `<li>"Stake is ready!"</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
   });
 // Iteration 3 using async/await
 async function makeBroccoli() {
@@ -143,10 +144,32 @@ async function makeBroccoli() {
     html.innerHTML += `<li>${await obtainInstruction("broccoli", 5)}</li>`;
     html.innerHTML += `<li>${await obtainInstruction("broccoli", 6)}</li>`;
     html.innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
   } catch (err) {
     console.log("Broccoli error", err);
   }
 }
 makeBroccoli();
-// Bonus 2 - Promise all
-// ...
+
+const prom1 = obtainInstruction("brusselsSprouts", 0);
+const prom2 = obtainInstruction("brusselsSprouts", 1);
+const prom3 = obtainInstruction("brusselsSprouts", 2);
+const prom4 = obtainInstruction("brusselsSprouts", 3);
+const prom5 = obtainInstruction("brusselsSprouts", 4);
+const prom6 = obtainInstruction("brusselsSprouts", 5);
+const prom7 = obtainInstruction("brusselsSprouts", 6);
+const prom8 = obtainInstruction("brusselsSprouts", 7);
+
+Promise.all([prom1, prom2, prom3, prom4, prom5, prom6, prom7, prom8]).then(
+  (arr) => {
+    arr.forEach((value) => {
+      document.querySelector(
+        "#brusselsSprouts"
+      ).innerHTML += `<li>${value}</li>`;
+    });
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+    document.querySelector(
+      "#brusselsSprouts"
+    ).innerHTML += `<li>Brussels Sprouts are ready!</li>`;
+  }
+);
